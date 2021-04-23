c28c71e0beedf61e0dd53bb1cb9d26533f4910e0
safak <safakk.bilici@gmail.com>
2021-04-23 18:57:45 +0300

    k fold related mkdirs

--- utils/file.py
+++ utils/file.py
@@ -1,8 +1,11 @@
 import json, os, cv2
 import skimage.io as io
+import numpy as np
 
 
 def get_dirs():
+    if not os.path.isdir("./kfold"):
+        os.mkdir("./kfold")
     with open("./file_config.json") as json_f:
         dirs = json.load(json_f)
     for dir in dirs["files"][0]:
@@ -35,3 +38,24 @@ def saveResult_drive(save_path,npyfile,flag_multi_class = False,num_class = 2):
     for i,item in enumerate(npyfile):
         img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
         io.imsave(os.path.join(save_path,"%d.png"%(i)),img)
+
+
+def labelVisualize(num_class,color_dict,img):
+    img = img[:,:,0] if len(img.shape) == 3 else img
+    img_out = np.zeros(img.shape + (3,))
+    for i in range(num_class):
+        img_out[img == i,:] = color_dict[i]
+    return img_out / 255def labelVisualize(num_class,color_dict,img):
+    img = img[:,:,0] if len(img.shape) == 3 else img
+    img_out = np.zeros(img.shape + (3,))
+    for i in range(num_class):
+        img_out[img == i,:] = color_dict[i]
+    return img_out / 255
+
+def saveResult(fold,k,save_path,npyfile,flag_multi_class = False,num_class = 2):
+    for i,item in enumerate(npyfile):
+        img = labelVisualize(num_class,COLOR_DICT,item) if flag_multi_class else item[:,:,0]
+        io.imsave(os.path.join(save_path,"%d_predict.png"%(i+fold*k)),img)
+
+
+
